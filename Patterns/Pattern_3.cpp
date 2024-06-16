f5dfbfc366cb9a043dcecf242a330af77c9e0688
githubKushagra <devkussu43@gmail.com>
2024-06-17 00:12:56 +0530

    committed new patterns

---
+++ Patterns/Pattern_3.cpp
@@ -0,0 +1,29 @@
+/*
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+    1 2 3 4 5
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    for(int i=0 ; i<n ; i++) {
+        for(int j = 0 ; j<=i ; j++) {
+            cout << j+1 << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    printPattern(n);
+
+    return 0;
+}
