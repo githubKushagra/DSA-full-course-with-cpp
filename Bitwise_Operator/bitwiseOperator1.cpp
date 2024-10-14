8a49036e04988240384655154d2f4aee902b451c
githubKushagra <devkussu43@gmail.com>
2024-10-14 22:43:03 +0530

    Working On bitwise Operator.

---
+++ Bitwise_Operator/bitwiseOperator1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int a = 5;
+    int b = 7;
+
+    cout << "a&b : " << (a&b) << endl;
+    cout << "a|b : " << (a|b) << endl;
+    cout << "~a : " << (~a) << endl;
+    cout << "a^b : " << (a^b) << endl;
+}
